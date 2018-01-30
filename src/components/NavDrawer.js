890e1ff48c61cece35e5045c1dd596ec7764caa3
rob dev <robthewebdev@gmail.com>
2018-01-30 15:03:24 -0600

    added drawer open/close

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -6,13 +6,38 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Menu from 'material-ui/svg-icons/navigation/menu';
 
 class NavDrawer extends Component {
+    state = {
+        open: false
+    }
+
+    toggle = () => {
+       this.setState((prevState, props) => {
+           return {
+                open: !prevState.open
+           }
+       })
+    }
+
     render() {
         return (
             <div>
-                <FloatingActionButton>
+                <FloatingActionButton
+                    onTouchTap={this.toggle}
+                >
                     <Menu/>
                 </FloatingActionButton>
-                <Drawer>
+                <Drawer
+                    open={this.state.open}
+                >
+                    <div
+                        style={{
+                            height: '200px',
+                            width: '100%',
+                            backgroundColor: 'salmon'
+                        }}
+                    >
+                        logincontainer
+                    </div>
                 <Divider/>
                     <MenuItem
                         primaryText={'Play'}
