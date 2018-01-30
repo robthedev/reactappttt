298ff4bf673b6e82d604c0b3b2e086f4f812da60
rob dev <robthewebdev@gmail.com>
2018-01-30 16:17:20 -0600

    added responsive components

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import Menu from 'material-ui/svg-icons/navigation/menu';
+import { Link } from "react-router";
+import { NavToggleButton } from '../styled/NavDrawer';
 
 class NavDrawer extends Component {
     state = {
-        open: false
+        open: true,
+        width: 250
     }
 
     toggle = () => {
@@ -21,13 +22,14 @@ class NavDrawer extends Component {
     render() {
         return (
             <div>
-                <FloatingActionButton
-                    onTouchTap={this.toggle}
-                >
-                    <Menu/>
-                </FloatingActionButton>
+               <NavToggleButton
+                    toggle={this.toggle}
+                    width={this.state.width}
+                    open={this.state.open}
+               />
                 <Drawer
                     open={this.state.open}
+                    width={this.state.width}
                 >
                     <div
                         style={{
@@ -39,12 +41,22 @@ class NavDrawer extends Component {
                         logincontainer
                     </div>
                 <Divider/>
-                    <MenuItem
-                        primaryText={'Play'}
-                    />
-                    <MenuItem
-                        primaryText={'Profile'}
-                    />
+                    <Link
+                        to={'/'}
+                    >
+                        <MenuItem
+                            primaryText={'Play'}
+                            onTouchTap={this.toggle}
+                        />
+                    </Link>
+                    <Link
+                    to={'/profile'}
+                    >
+                        <MenuItem
+                            primaryText={'Profile'}
+                            onTouchTap={this.toggle}
+                        />
+                    </Link>
                 </Drawer>
             </div>
         )
