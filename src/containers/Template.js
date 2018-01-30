e1c9d1f321abbb1fe4cff9fdf2e696b5716d9aa3
rob dev <robthewebdev@gmail.com>
2018-01-30 14:27:30 -0600

    added navdrawer component

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import RaisedButton from 'material-ui/RaisedButton';
+import NavDrawer from '../components/NavDrawer';
 
 injectTapEventPlugin();
 
@@ -10,10 +10,7 @@ class Template extends Component {
         return (
             <MuiThemeProvider>
                 <div>
-                    <header>
-                        <h1>mitel header</h1>
-                        <RaisedButton label="Default" />
-                    </header>
+                    <NavDrawer />
                     <main>
                         {this.props.children}
                     </main>
