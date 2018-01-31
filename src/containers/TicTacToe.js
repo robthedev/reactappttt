d460b8fe7d49def0da2eb510aa7bbc186acf0dd7
rob dev <robthewebdev@gmail.com>
2018-01-31 15:28:42 -0600

    ai move is working, game can be completed

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -52,7 +52,32 @@ class TicTacToe extends Component {
     }
 
     move = (index, marker) => {
-        console.log('Move made', marker, index);
+        this.setState( (prevState, prop) => {
+            let {gameState, yourTurn, gameOver, winner} = prevState
+            yourTurn = !yourTurn
+            gameState.splice(index, 1, marker);
+
+            let foundWin = this.winChecker(gameState);
+
+            if (foundWin) {
+                winner = gameState[foundWin[0]];
+            }
+            if (foundWin || !gameState.includes(false)) {
+                gameOver = true;
+            }
+
+            if (!yourTurn && !gameOver) {
+                this.makeAiMove(gameState);
+            }
+
+            return {
+                gameState,
+                yourTurn,
+                gameOver,
+                win: foundWin || false,
+                winner
+            }
+        })
     }
 
     makeAiMove = (gameState) => {
@@ -65,7 +90,11 @@ class TicTacToe extends Component {
             }
         })
         let aiMove = openSquares[this.random(0, openSquares.length)];
-        this.move(aiMove, otherMark);
+        
+        setTimeout( () => {
+            this.move(aiMove, otherMark);
+        }, 2000)
+       
     }
 
     random = (min, max) => {
